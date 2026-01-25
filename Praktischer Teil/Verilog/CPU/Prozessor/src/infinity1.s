7ce6e1e9b8f90462bc4991c733a302d321d9051d
Christian Plessl <christian.plessl@uni-paderborn.de>
2026-01-25 16:54:19 +0100

    Move to different location (apps directory)

--- Praktischer Teil/Verilog/CPU/Prozessor/src/infinity1.s
+++
@@ -1,5 +0,0 @@
-begin:
-    addi x1, x0, 1
-    xor x0, x0, x0
-    xor x0, x0, x0
-    j begin
--- Praktischer Teil/Verilog/CPU/Prozessor/src/infinity2.s
+++
@@ -1,5 +0,0 @@
-begin: addi x1, x1, 1
-    andi x2, x1, 1
-    beq x2, x0, skip
-    xor x0, x0, x0
-skip: j begin
