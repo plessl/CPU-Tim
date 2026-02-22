301eb857da09ed36d1910d79fc34b0cecd96f501
Christian Plessl <christian.plessl@uni-paderborn.de>
2026-02-22 19:48:16 +0100

    chore: Cleanup code

--- PraktischerTeil/Verilog/CPU/Prozessor/src/apps/snake-simple-fcalltest/main.c
+++ PraktischerTeil/Verilog/CPU/Prozessor/src/apps/snake-simple-fcalltest/main.c
@@ -5,7 +5,6 @@
 #define BTN_DOWN  0x4000
 #define BTN_RIGHT 0x2000
 #define BTN_UP    0x1000
-#define BTN_START 0x0800
 
 #define COLOR_BLACK 0u
 #define COLOR_GREEN 2u
@@ -15,47 +14,9 @@
 volatile unsigned int * const spi = (volatile unsigned int * ) 0x00030000;
 volatile unsigned int * const fb  = (volatile unsigned int * ) 0x00020000;
 
-enum dir{
-	up = 1,
-	down = 2,
-	left = 3,
-	right = 4
-};
-
-int x, y;
-int x_n, y_n;
-int dir_l;
-
-void fb_write(unsigned row, unsigned col, unsigned value) {
-    fb[row * 64 + col] = value;
+void fb_write(unsigned row, unsigned col, unsigned color) {
+    fb[row * 64 + col] = color;
 }
-
-
-void move(){
-	int buttons = spi[0];
-	x_n = x;
-	y_n = y;
-
-	if(buttons & BTN_DOWN){
-		y_n = y - 1;
-		x_n = x;
-	}
-	if(buttons & BTN_LEFT){
-		x_n = x - 1;
-		y_n = y;
-	}
-	if(buttons & BTN_RIGHT){
-		x_n = x + 1;
-		y_n = y;
-	}
-	if(buttons & BTN_UP){
-		y_n = y + 1;
-		x_n = x;
-	}
-}
-
-
-
 
 void init_display(unsigned color){
 	for(int i = 0; i < SCREEN_H; i++){
@@ -65,9 +26,6 @@ void init_display(unsigned color){
 	}
 }
 
-// TODO: change and calibrate function such that parameter is in milliseconds and calculate count based on CPI and clock frequency
-// CPI = 6
-// f = 50 MHz
 __attribute__((noinline)) void param_delay(unsigned int count) {
 	for(unsigned int i = 0; i < count; i++) {
 		__asm__ volatile("nop");
@@ -75,17 +33,17 @@ __attribute__((noinline)) void param_delay(unsigned int count) {
 }
 
 int main(){
-	// Funktionierende Button-Steuerung
 	unsigned int px = 32;
 	unsigned int py = 32;
 	int dx = 0;
 	int dy = 0;
 	unsigned int buttons;
 	
-	init_display(2);
+	init_display(COLOR_BLACK);
+
 	while(1){
+
 		// Pixel an
-		//fb[py * 64 + px] = 7;
 		fb_write(py, px, 7);
 		param_delay(30000);
 		
